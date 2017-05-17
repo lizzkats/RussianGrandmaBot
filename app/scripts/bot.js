6715aa3428c5b93388fadfb7656ae4d4d8657f9f
lizzkats <katsnelson.lizz@gmail.com>
2017-05-16 17:34:44 -0700

    in soviet russia git commit you

--- app/scripts/bot.js
+++ app/scripts/bot.js
@@ -1,13 +1,31 @@
-function getRandomNum(min, max) {
+const lecture = [
+  {lecture: 'Я устала от тебя', translation: 'I am tired of you.'},
+  {lecture: 'иди к черту', translation: 'Go to hell!'},
+  {lecture: 'Я тебе не хрен с горы!', translation: 'I am not a stranger! Literal: I am not horseradish from the mountains' },
+]
+let currentLecture = getLecture()
+let currentTranslation = currentLecture.translation
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
-const lecture = ['Я устала от тебя', 'позор тебе', 'иди к черту
-']
+  function getLecture() {
+    for (let i = 0; i < lecture.length; i++) {
+      return lecture[getRandomInt(0, 3)]
+    }
+  }
 
-var random = Math.floor(Math.random()* lecture.length);
-var lectureMe = lecture[random]
+  function lectureMe(){
+    return responsiveVoice.speak(currentLecture.lecture, "Russian Female");
+  }
 
-$('button').click(function(){
-responsiveVoice.speak(lectureMe, "Russian Female");
 
+$(document).ready(function(){
+
+  $('button').click(function(){
+    lectureMe()
+  })
+  $('.translation').append('<h1>' + currentLecture.translation + '</h1>')
 })
