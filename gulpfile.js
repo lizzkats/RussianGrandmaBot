caa95b6207024f71a1ccfa6d96513a2c755c45d9
lizzkats <katsnelson.lizz@gmail.com>
2017-05-18 09:45:49 -0700

    in soviet russia app deploy you

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const browserSync = require('browser-sync').create();
 const del = require('del');
 const wiredep = require('wiredep').stream;
 const runSequence = require('run-sequence');
+const ghPages = require('gulp-gh-pages');
+
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
@@ -90,6 +92,11 @@ gulp.task('extras', () => {
   ], {
     dot: true
   }).pipe(gulp.dest('dist'));
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
 });
 
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
